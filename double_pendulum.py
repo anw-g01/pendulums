6e84d3f57ab6ce14faac941628e774aae3893409
Anwarat Gurung <agurung_01@outlook.com>
2025-06-01 19:55:06 +0100

    Create a full animation writer function for the double pendulum.

--- double_pendulum.py
+++ double_pendulum.py
@@ -2,9 +2,12 @@ import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
+from matplotlib.animation import FuncAnimation, FFMpegWriter
 plt.rcParams["font.size"] = 8
 plt.rcParams["font.family"] = "monospace"
 plt.rcParams["lines.linewidth"] = 0.8
+from tqdm_pbar import tqdmFA
+import datetime
 
 
 def dp_sim(
@@ -70,7 +73,7 @@ def dp_sim(
     success = sol.success
     print(f"\nsolver success: {success} ({sol.nfev:,} nfev)")
     print(f"t.shape: {t.shape}, Z.shape: {Z.shape}\n")
-    p = [r1, r2, m1, m2, g, theta1_0, theta2_0, w1_0, w2_0] # also return vector of essential parameters
+    p = [r1, r2, m1, m2, g, T, theta1_0, theta2_0, w1_0, w2_0] # also return vector of essential parameters
 
     return t, Z, p
 
@@ -157,7 +160,7 @@ def plot_graphs(
     plt.show()
 
 
-def plot_pos(
+def plot_frames(
         t, Z, p,
         m1_colour: str = "tab:green",
         m2_colour: str = "tab:red",
@@ -255,28 +258,221 @@ def plot_pos(
     plt.show()
 
 
-if __name__ == "__main__":
-
-    # ----- EXAMPLE USAGE ----- #
-    
-    t, Z, p = dp_sim(
-        r1=0.5,
-        r2=0.5,
-        m1=0.5,
-        m2=0.25,
-        theta1_0=35,
-        theta2_0=65
+def animate_dp(
+        t, Z, p,
+        frames_per_second: int = 60,
+        dots_per_inch: int = 200,
+        m1_colour: str = "tab:green",
+        m2_colour: str = "tab:red",
+        link1_colour: str = "black",
+        link2_colour: str = "black",
+        m1_markersize: float = 150,
+        m2_markersize: float = 150,
+        line_width: float = 0.65,
+        trail_linewidth: float = 1,
+        trail_length_pct: float = 5,
+        origin: tuple = (0, 0),
+        figure_size: tuple = (8, 7),
+        grid_alpha: float = 0.1,
+        n_max_ticks: int = 4,
+        x_axis_limits: tuple = None,
+        y_axis_limits: tuple = None,
+        max_axis_extent: float = 1.2,
+        show_legend: bool = False
+    ) -> None:
+    """
+    Animate the double pendulum using matplotlib's FuncAnimation.
+    """
+    # check first and last frames + figure size before animatino writing process:
+    print(f"<CLOSE FIGURE WINDOW TO START ANIMATION WRITING>")
+    plot_frames(
+        t, Z, p,
+        m1_colour=m1_colour, m2_colour=m2_colour,
+        link1_colour=link1_colour, link2_colour=link2_colour,
+        m1_markersize=m1_markersize, m2_markersize=m2_markersize,
+        line_width=line_width, figure_size=figure_size,
+        grid_alpha=grid_alpha, n_max_ticks=n_max_ticks,
+        x_axis_limits=x_axis_limits, y_axis_limits=y_axis_limits,
+        max_axis_extent=max_axis_extent, show_legend=show_legend,
+        show_trail=True,    
+        trail_length_pct=100,   # show full trails from start to finish
+        draw_start=True,        # draw first frame
+        draw_end=True,          # draw last frame
     )
 
-    plot_graphs(t, Z, p)
+    interval = int(1000 / frames_per_second)    # convert FPS to milliseconds
+    steps = len(t)    # total number of time steps
+    trail_length = int((trail_length_pct / 100) * steps) 
+    print(f"\n{steps:,} steps @ {frames_per_second} fps (~{interval * 1e-3:.3f} sec/frame)")
+    print(f"writing {steps} frames to MP4...\n")
 
-    square_limits = 1.2
-    plot_pos(
+    r1, r2, m1, m2, g, T, *_ = p
+    theta1, theta2, w1, w2 = Z[0], Z[1], Z[2], Z[3]
+
+    # array of coordinates of masses (bobs):
+    x0, y0 = origin
+    x1 = r1 * np.sin(theta1) + x0
+    x2 = x1 + r2 * np.sin(theta2)
+    y1 = - r1 * np.cos(theta1) + y0
+    y2 = y1 - r2 * np.cos(theta2)
+
+    # ----- FIGURE ----- #
+    fig, ax = plt.subplots(figsize=figure_size)
+    ax.xaxis.set_major_locator(MaxNLocator(n_max_ticks))
+    ax.yaxis.set_major_locator(MaxNLocator(n_max_ticks))
+    ax.set_xlabel(r"$x$ ($m$)")
+    ax.set_ylabel(r"$y$ ($m$)")
+    ax.set_aspect("equal")
+    # grids and dashed lines
+    ax.grid(True, alpha=grid_alpha)
+    dashed_alpha, dashed_linewidth = 0.1, 1
+    hline = ax.axhline(0, linestyle="--", color="black", alpha=dashed_alpha, linewidth=dashed_linewidth)
+    vline = ax.axvline(0, linestyle="--", color="black", alpha=dashed_alpha, linewidth=dashed_linewidth)
+    hline.set_dashes([10, 10]), vline.set_dashes([10, 10])
+    ax.scatter(x0, y0, marker="o", color="black", s=20)    # origin pivot
+
+    # plot elements to be updated
+    link1, = ax.plot([], [], color=link1_colour, linewidth=line_width, zorder=1)
+    link2, = ax.plot([], [], color=link2_colour, linewidth=line_width, zorder=2)
+    mass1 = ax.scatter([], [], marker="o", color=m1_colour, s=m1_markersize, zorder=3)
+    mass2 = ax.scatter([], [], marker="o", color=m2_colour, s=m2_markersize, zorder=3)
+    if trail_length_pct > 0:    # show trails for masses
+        m1_trail, = ax.plot([], [], color=m1_colour, linewidth=trail_linewidth, zorder=0)
+        m2_trail, = ax.plot([], [], color=m2_colour, linewidth=trail_linewidth, zorder=0)
+
+    # --- AXIS LIMITS & LEGEND --- #
+    # independent overrides (if only one set of limits is provided):
+    if x_axis_limits:
+        ax.set_xlim(x_axis_limits)
+    else:
+        x_extent = max_axis_extent * np.max(np.abs(x2))
+        ax.set_xlim(-x_extent, x_extent)
+    if y_axis_limits:
+        ax.set_ylim(y_axis_limits)
+    else:
+        y_extent = max_axis_extent * np.max(np.abs(y2))
+        ax.set_ylim(-y_extent, y_extent)
+    if show_legend:
+        ax.legend(loc="best")
+
+    # ----- PROGRESS BAR ----- #
+    pbar = tqdmFA(total=len(t))
+    
+    # --- ANIMATION FUNCTION SETUP --- #
+    def init():
+        link1.set_data([], []), link2.set_data([], [])
+        mass1.set_offsets([x1[0], y1[0]]), mass2.set_offsets([x2[0], y2[0]])
+        if trail_length_pct > 0:
+            m1_trail.set_data([], []), m2_trail.set_data([], [])
+            return link1, link2, mass1, mass2, m1_trail, m2_trail
+        return link1, link2, mass1, mass2
+
+    def update(frame):
+        # update link and mass positions:
+        link1.set_data([x0, x1[frame]], [y0, y1[frame]])
+        link2.set_data([x1[frame], x2[frame]], [y1[frame], y2[frame]])
+        mass1.set_offsets([x1[frame], y1[frame]])
+        mass2.set_offsets([x2[frame], y2[frame]])
+        # update trails if applicable:
+        if trail_length_pct > 0:
+            i0 = max(0, frame - trail_length)   # start index for the trail
+            m1_trail.set_data(x1[i0: frame + 1], y1[i0: frame + 1])
+            m2_trail.set_data(x2[i0: frame + 1], y2[i0: frame + 1])
+            pbar.update(1)
+            return link1, link2, mass1, mass2, m1_trail, m2_trail
+        pbar.update(1)    # else
+        return link1, link2, mass1, mass2
+
+    ani = FuncAnimation(
+        fig=fig,
+        func=update,
+        frames=range(len(t)),
+        init_func=init,
+        interval=interval,
+        repeat=False,
+        blit=True,
+    )
+
+    writer = FFMpegWriter(
+        fps=frames_per_second,
+        bitrate=30_000,
+    )
+
+    angle1, angle2 = np.degrees(theta1[0]), np.degrees(theta2[1])
+    file_name = (
+        "dp_sim_"
+        f"θ1={angle1:.0f}_θ2={angle2:.0f}_"
+        f"{steps=:,}({T=})_@{trail_length_pct}%_"
+        f"dpi={dots_per_inch}_"
+        f"{m1=:.1f}_{m2=:.1f}_"
+        f"{r1=:.1f}_{r2=:.1f}_"
+        ".mp4"
+    )
+
+    ani.save(filename=file_name, writer=writer, dpi=dots_per_inch)
+    pbar.close()    # close progress bar
+    print(f"\nanimation saved as '{file_name}.mp4'")
+
+    # --- REPORT --- #
+    elapsed = int(pbar.format_dict["elapsed"])
+    t = datetime.timedelta(seconds=elapsed)
+    print(f"\ntotal elapsed time: {t}")
+
+    avg_iter_per_sec = steps / t.total_seconds()
+    if 1 / avg_iter_per_sec < 1:
+        avg_rate = f"{1 / avg_iter_per_sec * 1e3:.0f} ms/frame"
+    else:
+        avg_rate = f"{1 / avg_iter_per_sec:.2f} sec/frame"
+    print(f"{avg_iter_per_sec:.1f} frames/sec processed ({avg_rate})")
+
+    return None
+
+
+if __name__ == "__main__":
+
+    # ----- EXAMPLE FRAME PLOT ----- #
+    
+    # t, Z, p = dp_sim(
+    #     r1=0.5,
+    #     r2=0.5,
+    #     m1=0.5,
+    #     m2=0.25,
+    #     theta1_0=35,
+    #     theta2_0=65
+    # )
+
+    # plot_frames(t, Z, p)
+
+    # square_limits = 1.2
+    # plot_pos(
+    #     t, Z, p,
+    #     x_axis_limits=(-square_limits, square_limits),
+    #     y_axis_limits=(-square_limits, 0.5),
+    #     # draw_start=True,
+    #     # draw_end=True
+    #     n_frames=5,
+    #     # show_trail=True
+    # )
+
+    # ----- EXAMPLE ANIMATION ----- #
+
+    t, Z, p = dp_sim(
+        r1=0.5, r2=0.5,
+        m1=0.3, m2=0.3,
+        theta1_0=55, theta2_0=130,
+        T=10, steps=600,
+    )
+
+    animate_dp(
         t, Z, p,
-        x_axis_limits=(-square_limits, square_limits),
-        y_axis_limits=(-square_limits, 0.5),
-        # draw_start=True,
-        # draw_end=True
-        n_frames=5,
-        # show_trail=True
+        m1_markersize=200,
+        m2_markersize=200,
+        line_width=0.65,
+        trail_linewidth=0.5,
+        trail_length_pct=0,     # no trails
+        figure_size=(10, 10),
+        x_axis_limits=(-1.2, 1.2),
+        y_axis_limits=(-1.2, 0.5),
+        max_axis_extent=1.2,
+        dots_per_inch=200,
     )
