ddce13105047a5eaa2bcdf707eb6a66b9feab867
Anwarat Gurung <agurung_01@outlook.com>
2025-06-11 23:51:22 +0100

    Add a video duration stat to showcase number of video seconds written to MP4 with FuncAnimation writing.

--- tqdm_pbar.py
+++ tqdm_pbar.py
@@ -7,28 +7,38 @@ class tqdmFA(tqdm):
     """
     A custom `tqdm` progress bar designed for visualising 
     frame-by-frame generation progress with `matplotlib.animation.FuncAnimation`.
+
+    Args:
+        fps (int): Frames per second of the video file.
+
+    `|███████------------------| 30.7% | frame: 168/548 | video duration: 0:02/0:09 | 40 ms/frame [00:06<00:15, 25.3 frames/sec]`
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, fps: int = 60, **kwargs):
         bar_format_str = (
-            "|{bar:30}| {percentage:.1f}% | frame: {n_fmt}/{total_fmt}"
+            "|{bar:30}| {percentage:.1f}%"
+            " | frame: {n_fmt}/{total_fmt}"
+            " | video duration: {vid_dur}"
             " | {sec/frame} "
             "[{elapsed}<{remaining}, {frames/sec}]"
         )
         params = {
             "bar_format": bar_format_str,
             "ascii": "-█",
-            "colour": "cyan"   # progress bar colour
+            "colour": "yellow"   # progress bar colour
         }
+        self.fps = fps      # frames per second of MP4 video file
         for key, value in params.items():
             kwargs.setdefault(key, value)
         super().__init__(*args, **kwargs)       # pass to constructor of parent class
 
     @property
-    def format_dict(self):
+    def format_dict(self) -> dict:
+        """Override the format_dict property to include custom formatting for video writing progress."""
         d = super().format_dict
         d["n_fmt"] = f"{d['n']:,}" if d["n"] else "?"                   # current frame number
         d["total_fmt"] = f"{d['total']:,}" if d["total"] else "?"       # total frames to write (process)
+        # written frames/sec and time per frame:
         if d["rate"]:
             rate = d["rate"]
             # time taken to write a frame:
@@ -41,6 +51,17 @@ class tqdmFA(tqdm):
         else:
             d["sec/frame"] = "? sec/frame"
             d["frames/sec"] = "? frames/sec"
+        # written video duration in seconds:
+        def fmt(t) -> None:
+            """Format timedelta as M:SS (not H:MM:SS)"""
+            minutes, seconds = divmod(t.seconds, 60)
+            return f"{minutes}:{seconds:02}"
+        if self.n and self.total:
+            elapsed = datetime.timedelta(seconds=self.n/self.fps)         # total steps / FPS
+            duration = datetime.timedelta(seconds=self.total/self.fps)    # total video duration
+            d["vid_dur"] = f"{fmt(elapsed)}/{fmt(duration)}"
+        else:
+            d["vid_dur"] = f"?:??/?:??"
         return d
 
 
@@ -48,7 +69,7 @@ if __name__ == "__main__":
 
     # --- DUMMY LOOP --- #
 
-    total_frames = 100      # frames to write to video writer (e.g. ffmpeg or pillow)
+    total_frames = 548      # frames to write to video writer (e.g. ffmpeg or pillow)
 
     pbar = tqdmFA(iterable=range(total_frames))
     for _ in pbar:
